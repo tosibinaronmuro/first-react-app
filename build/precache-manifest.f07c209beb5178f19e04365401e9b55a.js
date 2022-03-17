ed234ca06276c37a9474cce0c56b7dfacfa6ff0a
tosiron <67600752+tosibinaronmuro@users.noreply.github.com>
2022-03-17 14:01:34 +0100

    Add files via upload

---
+++ build/precache-manifest.f07c209beb5178f19e04365401e9b55a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "939ee477dc205c4ca515",
+    "url": "/static/css/main.de3a65aa.chunk.css"
+  },
+  {
+    "revision": "939ee477dc205c4ca515",
+    "url": "/static/js/main.939ee477.chunk.js"
+  },
+  {
+    "revision": "d8a9eab682832ca21c76",
+    "url": "/static/js/1.d8a9eab6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "872d2f0fb990c54584335165078f5b87",
+    "url": "/index.html"
+  }
+];
